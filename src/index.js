0f1968d467f2e064dc07864356ea7f5e8d07b0fa
fluffy29 <hmajzoub12@gmail.com>
2025-03-17 10:57:18 +0100

    Implement user routes and middleware for request logging; update root endpoint response

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const userRoutes = require("../src/routes/users");
+
+app.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  const number = Date.now();
+  req.number = now;
+  next();
+});
+
+// app.get("/", (req, res) => {
+//   res.send(req.number.toString());
+// });
+
+// app.get("/api/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send(req.requestTime.toString());
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -8,4 +26,8 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my API ! e-commerce backed 🤳");
 });
